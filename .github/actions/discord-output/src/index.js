3665c8b7438d18186df366547c653e190a8ab878
Stantrh <stanislas.troha8@etu.univ-lorraine.fr>
2024-10-10 18:47:20 +0200

    fin de l'action, reste README.md à faire

--- .github/actions/discord-output/src/index.js
+++ .github/actions/discord-output/src/index.js
@@ -5,16 +5,12 @@ async function run() {
   try {
     // On récupère les inputs du worfklow ci.yml
     const discordWebhook = core.getInput('discord_webhook');
-    let testStatus = core.getInput('test_status');
+    const testStatus = core.getInput('test_status');
     const jobName = core.getInput('job_name');
 
     // Au cas où quelqu'un utilise l'action et ne spécifie pas l'url du webhook dans les inputs
     if (!discordWebhook) {
       throw new Error('Il faut donner un webhook discord en entrée dans votre workflow !');
-    }
-
-    if(testStatus === ""){
-      testStatus = "Fail";
     }
 
     // On construit le message que le webhook va afficher
@@ -25,8 +21,8 @@ async function run() {
       avatar_url: 'https://ipfs.io/ipfs/QmcoYqrddqLcfDa2q6iA4X2i4FMAjEAEGNPxSi1oNWjfCZ/nft.jpg',
       embeds: [
         {
-          title: `🚀 WORKFLOW **${nomWorkflow}**`,
-          description: `**Job:** *${jobName}\n**Status:** ${statusEmoji} ${testStatus.charAt(0).toUpperCase() + testStatus.slice(1)}*\n\n:sparkles: Le workflow s'est bien complété ! :sparkles:`,
+          title: `🚀 WORKFLOW : **${nomWorkflow}**`,
+          description: `**Job:** *${jobName}*\n**Status:** ${statusEmoji} ${testStatus.charAt(0).toUpperCase() + testStatus.slice(1)}\n\n:sparkles: Le workflow s'est bien complété ! :sparkles:`,
           color: testStatus === 'success' ? 3066993 : 15158332,  // Vert si succès, rouge sinon
           fields: [
             { 
